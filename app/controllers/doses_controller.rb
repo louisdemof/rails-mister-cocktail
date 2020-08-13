509ba889a882baeefb50f1ab0bba42593f2c667f
Louis de Moffarts <louisdemof@gmail.com>
2020-08-13 16:07:36 +0200

    Added the Doses and can now create real Cocktails

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+before_action :find_cocktail
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning up database'
+Cocktail.destroy_all
+Ingredient.destroy_all
+puts 'database is clean'
+puts 'creating Ingredients'
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "syrup")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "Cognac" )
+Ingredient.create(name: "Coca Cola")
+Ingredient.create(name: "Aperol")
+Ingredient.create(name: "Spritz")
+Ingredient.create(name: "Prosecco")
+puts 'creating Cocktails'
+Cocktail.create(name: "Aperol Spritz")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Rom Coke")
+puts 'done'
